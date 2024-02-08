f2fb883d09b8dcdc2f66dafbeb808c11a583c1a5
Maja-Gigi <maja.hreljak@gmail.com>
2024-02-08 12:13:06 +0100

    Generatin HTML added products.js to the script element

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,21 +1,3 @@
-const products = [{
-  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating: {
-    stars: 4.5,
-    count: 87
-  },
-  priceCents: 1090,
-}, {
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating: {
-    stars: 4.5,
-    count: 87
-  },
-  priceCents: 1090,
-  }];
-
 let productHTML = '';
 
 products.forEach((product) => {
